84f9b87142b7d2a2b70f8114e28a1cafa2bc2b3d
Kawsar Ahmed <kawsarahmed4148@gmail.com>
2022-12-12 13:38:01 +0600

    css added

--- js/countries.js
+++ js/countries.js
@@ -1,9 +1,31 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayCountries(data))
 }
 
+const displayCountries = countries => {
+    const countriesHtml = countries.map(country => getCountryHtml(country))
+    
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHtml.join(' ')
+}
 
+const getCountryHtml = country => {
+    console.log(country);
+    const{name, flags, area, capital} = country
+
+    return `
+    <div class="country">
+        <h2>${name.common}</h2>
+        <h3>${capital}</h3>
+        <p>${area}</p>
+        <img src=${flags.png}>
+
+    </div>
+
+
+    `
+}
 
 loadCountries()
